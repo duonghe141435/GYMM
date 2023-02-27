1645179f6b55c365334c72778274f63de45ce6b4
hieuhmhe141258 <106162342+hieuhmhe141258@users.noreply.github.com>
2023-02-27 15:59:36 +0700

    commit login validate

--- src/main/webapp/assets/js/loginValidate.js
+++ src/main/webapp/assets/js/loginValidate.js
@@ -1,190 +1,27 @@
-// Select all input elements for varification
-const name = document.querySelector('#exampleName');
-const emailElm = document.querySelector('#exampleInputEmail');
-const passwordElm = document.querySelector('#examplePasswordInput')
-const phoneNumber = document.querySelector('#examplePhoneNumber');
-const rePassword = document.querySelector('#exampleRepeatPasswordInput');
-const btnRegis = document.getElementById("btn-register");
-const form = document.querySelector('#register');
-
-form.addEventListener('register', function (e) {
-    e.preventDefault();
-});
-
-const isRequired = value => value === '' ? false : true;
-const checkLength = (length, min, max) => length < min || length > max ? false : true;
-const checkPass = (password) => {
-    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-    return re.test(password);
-};
-const checkMail = (email) => {
-  const em = new RegExp("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+");
-  return em.test(email);
-};
-
-const checkPhone = (phone) => {
-  const ph = new RegExp("^(((\\+|)84)|0)(3|5|7|8|9)+([0-9]{8})\\b");
-  return ph.test(phone);
-};
-
-const showError = (input, message) => {
-
-    const formField = input.parentElement;
-
-    formField.classList.remove('success');
-    formField.classList.add('error');
-    formField.classList.add("removeSpace");
-
-
-    const error = formField.querySelector('small');
-    error.textContent = message;
-};
-
-const showSuccess = (input) => {
-
-    const formField = input.parentElement;
-
-
-    formField.classList.remove('error');
-    formField.classList.add('success');
-
-    const error = formField.querySelector('small');
-    error.textContent = '';
+function validateForm() {
+    // Get the value of the input field with id="email"
+    const email = document.getElementById("exampleInputEmail").value;
+    // Get the value of the input field with id="password"
+    const password = document.getElementById("exampleInputPassword").value;
+    // Regular expression to check if the email is in the correct format
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    // Check if the email is empty
+    if (email == "") {
+        alert("Email field must be filled out");
+        return false;
+    }
+    // Check if the email is in the correct format
+    if (!email.match(emailRegex)) {
+        alert("Please enter a valid email address");
+        return false;
+    }
+    // Check if the password is empty
+    if (password == "") {
+        alert("Password field must be filled out");
+        return false;
+    }
+    // Send a request to the server to check the user's credentials
+    // If the credentials are correct, return true to submit the form
+    // If the credentials are incorrect, display an error message and return false to prevent the form from being submitted
+    return true;
 }
-
-const checkUsername = () => {
-
-    let valid = false;
-    const min = 3,
-        max = 20;
-    const username = name.value.trim();
-
-    if (!isRequired(username)) {
-        showError(name, 'Username is required.');
-    } else if (!checkLength(username.length, min, max)) {
-        showError(name, `Username must be between ${min} and ${max} characters.`)
-    } else {
-        showSuccess(name);
-        valid = true;
-    }
-    return valid;
-}
-
-const checkPassword = () => {
-
-    let valid = false;
-
-    const password = passwordElm.value.trim();
-
-    if (!isRequired(password)) {
-        showError(passwordElm, 'Password is required.');
-    } else if (!checkPass(password)) {
-        showError(passwordElm, 'Password must has at least 8 characters that include at least 1 lowercase character, 1 uppercase characters, 1 number, and 1 special character in (!@#$%^&*)');
-    } else {
-        showSuccess(passwordElm);
-        valid = true;
-    }
-
-    return valid;
-};
-
-const checkConfirmPassword = () => {
-    let valid = false;
-
-    const confirmPassword = rePassword.value.trim();
-    const password = passwordElm.value.trim();
-
-    if (!isRequired(confirmPassword)) {
-        showError(rePassword, 'Confirm password is required.');
-    } else if (password !== confirmPassword) {
-        showError(rePassword, 'Confirm password does not match');
-    } else {
-        showSuccess(rePassword);
-        valid = true;
-    }
-
-    return valid;
-};
-
-const checkEmail = () => {
-    let valid = false;
-
-    const email = emailElm.value.trim();
-
-    if (!isRequired(email)) {
-        showError(emailElm, 'Email is required.');
-    } else if (!checkMail(email)) {
-        showError(emailElm, 'Email is not valid');
-    } else {
-        showSuccess(emailElm);
-        valid = true;
-    }
-
-    return valid;
-};
-
-const checkPhoneNumber = () => {
-  let valid = false;
-
-  const phone = phoneNumber.value.trim();
-
-    if (!isRequired(phone)) {
-        showError(phoneNumber, 'Phone Number is required.');
-    } else if (!checkPhone(phone)) {
-        showError(phoneNumber, 'Phone Number is not valid');
-    } else {
-        showSuccess(phoneNumber);
-        valid = true;
-    }
-
-    return valid;
-};
-
-form.addEventListener('submit', function (e) {
-
-    e.preventDefault();
-
-
-    let isUsernameValid = checkUsername(),
-        isPhoneValid = checkPhoneNumber(),
-        isEmailValid = checkEmail(),
-        isPasswordValid = checkPassword(),
-        isConfirmPasswordValid = checkConfirmPassword();
-
-    let isFormValid = isUsernameValid &&
-        isPhoneValid &&
-        isEmailValid &&
-        isPasswordValid &&
-        isConfirmPasswordValid;
-
-    if (isFormValid) {
-        let msg = document.getElementById('title');
-        msg.innerHTML = 'You have registered successfully';
-        msg.style.color = 'green';
-
-    }else {
-        let msg = document.getElementById('title');
-        msg.innerHTML = 'Please fill input correctly';
-        msg.style.color = 'red';
-    }
-});
-
-form.addEventListener('input', function (e) {
-    switch (e.target.id) {
-        case 'exampleName':
-            checkUsername();
-            break;
-        case 'examplePhoneNumber':
-            checkPhoneNumber();
-            break;
-        case 'exampleInputEmail':
-            checkEmail();
-            break;
-        case 'examplePasswordInput':
-            checkPassword();
-            break;
-        case 'exampleRepeatPasswordInput':
-            checkConfirmPassword();
-            break;
-    }
-});
