f2bef1f49ee14166ffbbf66dcc56caf272a31a35
HoangLuong1401 <98100298+HoangLuong1401@users.noreply.github.com>
2023-04-11 10:02:25 +0700

    commit : create LogUser.java

---
+++ src/main/java/swp12/gym/model/mapper/LogUserMapper.java
@@ -0,0 +1,20 @@
+package swp12.gym.model.mapper;
+
+import org.springframework.jdbc.core.RowMapper;
+import swp12.gym.model.entity.LogUser;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LogUserMapper implements RowMapper<LogUser> {
+    public LogUser mapRow(ResultSet resultSet, int i) throws SQLException {
+
+        LogUser logUser = new LogUser();
+        logUser.setU_id(resultSet.getInt("user_id"));
+        logUser.setType_log(resultSet.getInt("type_log"));
+        logUser.setContent(resultSet.getString("content"));
+        logUser.setDate_time_create(resultSet.getString("time"));
+
+        return logUser;
+    }
+}
